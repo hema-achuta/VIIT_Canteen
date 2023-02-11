cea99b00a5fbf2cbd1f42351ecbd8be964de541d
hema <v.hemaachyuta@gmail.com>
2023-02-11 22:59:05 +0530

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,174 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> About | VIIT  Canteen</title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/aboutus.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type = "text/css" href ="indexFooter.css">
+
+  <body>
+
+  
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">VIIT Canteen</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">FARMER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart 
+            (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+            </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+          
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> Customer login</a></li>
+              <li> <a href="managerlogin.php"> Farmer Login</a></li>
+
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+        </div>
+
+      </div>
+    </nav>
+<head>
+    
+        <h1 align="center"><p align="center">Eat, drink <font color="red"><strong>and live free.</strong></font> There’s no <font color="green"><strong><em>telling what might happen tomorrow</em>.</strong></font></p></h1>
+<img src="images/canteen4.jpg" width="35%;">
+        
+</body>
+<!-- Footer -->
+		<footer class="footer-distributed" style="background-color:black" id="aboutUs">
+		<center>
+			<h1 style="font: 35px calibri;">About Us</h1>  
+		</center>
+		<div class="footer-left">
+			<h3 style="font-family: 'Times New Roman', cursive;">VIIT canteen &copy; </h3>
+		<!--	<div class="logo">
+				<a href="index.php"><img src="images/canteen4.jpg" width="20px"></a>
+			</div>-->
+			<br />
+			<p style="font-size:20px;color:white">There is no sincere love than the love of food</p>
+			<br />
+		</div>
+
+		<div class="footer-center">
+			<div>
+				<i class="fa fa-map-marker"></i>
+				<p style="font-size:20px">VIIT canteen<span>Duvvada</span></p>
+			</div>
+			<div>
+				<i class="fa fa-phone"></i>
+				<p style="font-size:20px">123456789</p>
+			</div>
+			<div>
+				<i class="fa fa-envelope"></i>
+				<p style="font-size:20px"><a href="mailto:agroculture@gmail.com" style="color:white">v.hemaachyuta@gmail.com</a></p>
+			</div>
+		</div>
+
+		<div class="footer-right">
+			<p class="footer-company-about" style="color:white">
+				<span style="font-size:20px"><b>About VIIT canteen</b></span>
+				It’s fun to get together and have something good to eat at least once a day. That’s what human life is all about - enjoying things.
+			</p>
+			<div class="footer-icons">
+				<a  href="#"><i style="margin-left: 0;margin-top:5px;"class="fa fa-facebook"></i></a>
+				<a href="#"><i style="margin-left: 0;margin-top:5px" class="fa fa-instagram"></i></a>
+				<a href="#"><i style="margin-left: 0;margin-top:5px" class="fa fa-youtube"></i></a>
+			</div>
+		</div>
+
+	</footer>
+         
+</html>
---
+++ index1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+   body{
+       background-image: url("vin.jpg");
+       
+    } 
+    .container{
+        padding: 2rem;
+    }
+    .hungry{
+        justify-content: center;
+        margin: 15rem;
+        text-align: center;
+        color: lightcyan;
+    }
+    .hungry btns{
+        background: linear-gradient(30deg,lightblue,lightpink);
+        margin: 5px;
+        width: 8rem;
+        height: 2rem;
+        border-radius: 10px;
+        font-size: 15px;
+        border-color: black;
+        }
+</style>
+
+<body>
+    <div class="container">
+        <div class="hungry">
+            <h1>FEELING HUNGRY</h1>
+             <center><a class="btns" href="customerlogin.php" role="button" > Order Now </a></center>
+        </div>
+    </div>
+</body>
+</html>
